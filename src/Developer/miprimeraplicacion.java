a974438402dfbbf80bf880b453b8269b68880fed
guadalupe2022 <penafloresmorenaguadalupe@gmail.com>
2022-03-08 16:08:02 -0600

    Calculo

---
+++ src/Developer/miprimeraplicacion.java
@@ -0,0 +1,30 @@
+package Developer;
+
+public class miprimeraplicacion {
+  public static void main(String[] args)  {
+      String nombreCompleto, direccion, estadoCivil;
+      int edad;
+      double gastosDiarios;
+      char genero;
+      boolean poseeVehiculo;
+      
+      nombreCompleto = "Camila Esperanza Rosales";
+      direccion = "Calle de la armagura desvio la tristiza";
+      estadoCivil = "Soltera";
+      edad = 30;
+      genero = 'F';
+      gastosDiarios = 34.45;
+      poseeVehiculo = false;
+      
+      System.out.println("Nombre completo: " + nombreCompleto);
+      System.out.println("Direccion      : " + direccion);
+      System.out.println("estado civil   : " + estadoCivil);
+      System.out.println("Edad           : " + edad);
+      System.out.println("Gastos diarios : " + gastosDiarios);
+      System.out.println("Genero         : " + genero);
+      System.out.println("Posee Vehiculo : " + poseeVehiculo);
+      
+      
+     
+  }  
+}
